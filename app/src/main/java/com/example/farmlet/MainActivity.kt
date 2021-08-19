78f02e036a2c949f1b5268b22c07342a59e3d364
obedg21 <gwilliobed2k@gmail.com>
2021-08-18 19:27:48 -0600

    Splash Screen

--- app/src/main/java/com/example/farmlet/MainActivity.kt
+++ app/src/main/java/com/example/farmlet/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.farmlet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.farmlet.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // Adding viewBinding
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
